150bf94767f473b20a94b7711b3722b5610cfea9
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-07-31 11:20:25 +0700

    Update

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 <html lang="en">
 <head>
 <title>Hello World</title>
+<style>
+    .trang{
+        background-color: white;
+        width: 80%;
+        margin: 0 auto;
+        padding: 10px;
+    }
+</style>
   </head>
 <body>
-    <?php
-        echo "<h1>Hello World 3000000000!!!</h1>";
-    ?>
+    <div class="trang">
+        <div class="than">
+            <h1>Introduce myself</h1>
+
+        </div>
+
+    </div>
+
 </body>
 </html>
